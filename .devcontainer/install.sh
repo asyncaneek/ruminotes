0eada62b480c5ecf927bff07384abeb9172b495e
asyncaneek <45776865+asyncaneek@users.noreply.github.com>
2023-03-21 01:08:14 -0300

    Delete install.sh

--- .devcontainer/install.sh
+++
@@ -1,7 +0,0 @@
-rustup component add rls rust-analysis rust-src rustfmt clippy
-echo "Installing Rust tools for wasm..."
-rustup component add target add wasm32-unknown-unknown
-cargo install trunk
-cargo install cargo-watch
-cargo install cargo-edit
-cargo install mprocs 
