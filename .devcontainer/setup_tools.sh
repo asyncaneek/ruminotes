c36f68b5c6c7901ecf583c78f5f893f3358d60e7
asyncaneek <45776865+asyncaneek@users.noreply.github.com>
2023-03-21 04:12:31 +0000

    created setup tools

--- .devcontainer/setup_tools.sh
+++ .devcontainer/setup_tools.sh
@@ -1,6 +1,6 @@
 rustup component add rls rust-analysis rust-src rustfmt clippy
 echo "Installing Rust tools for wasm..."
-rustup component add target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown
 cargo install trunk
 cargo install cargo-watch
 cargo install cargo-edit
